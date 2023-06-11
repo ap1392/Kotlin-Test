6cfb7edc6aa79ada40480b6695b778ad2724ee62
Aditya Pathak <adityap029@gmail.com>
2023-06-11 11:44:42 -0400

    Added the button the user will use to see their Top 5 apps

--- app/src/main/java/com/example/top5apps/MainActivity.kt
+++ app/src/main/java/com/example/top5apps/MainActivity.kt
@@ -2,12 +2,26 @@ package com.example.top5apps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val startTime = System.currentTimeMillis() - 24 * 60 * 60 * 1000 // 24 hours ago
+        val endTime = System.currentTimeMillis() // current time
+        val usageCollector = UsageCollector();
+        val usageStatistics = usageCollector.getUsageStatistics(this, startTime, endTime)
+        usageStatistics.sortByDescending { it.timeForeground }
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            println("Hello World")
+        }
 
+//        val test = usageStatistics.get(0);
+//
+//        println(usageStatistics);
     }
 }
