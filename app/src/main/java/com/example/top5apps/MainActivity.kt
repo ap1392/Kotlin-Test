eae035d5f0ac43721ab2f837615b13ada7431570
Aditya Pathak <adityap029@gmail.com>
2023-06-14 12:30:35 -0400

    Added top 5 apps users have used

--- app/src/main/java/com/example/top5apps/MainActivity.kt
+++ app/src/main/java/com/example/top5apps/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.top5apps
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import java.util.Objects
 
 
@@ -16,7 +17,9 @@ class MainActivity : AppCompatActivity() {
         val usageCollector = UsageCollector();
         val usageStatistics = usageCollector.getUsageStatistics(this, startTime, endTime)
         usageStatistics.sortByDescending { it.timeForeground }
+        val top5AppsTextView = findViewById<TextView>(R.id.top5Apps)
         val button: Button = findViewById(R.id.button)
+        val top5AppsPlaceholder = getString(R.string.top5_apps_placeholder)
         button.setOnClickListener {
             for (i in 0 until 5) {
                 if (!Objects.isNull(usageStatistics[i])) {
@@ -25,6 +28,11 @@ class MainActivity : AppCompatActivity() {
                     val applicationLabel = mostUsedApp.applicationlabel
                     val timeForeground = mostUsedApp.timeForeground
                     val launchCount = mostUsedApp.launchCount
+                    val existingText = top5AppsTextView.text.toString()
+                    val curApp = i + 1;
+                    val formattedText = existingText + applicationLabel + "\n"
+                    top5AppsTextView.text = formattedText
+
                 }
             }
 
